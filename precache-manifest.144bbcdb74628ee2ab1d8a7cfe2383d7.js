5529b0cd5e734590e8e37b6fa0a417ca4225d989
Matthew Panizza <38818691+ShrillHarrier@users.noreply.github.com>
2020-06-14 16:52:12 -0400

    Updates

---
+++ precache-manifest.144bbcdb74628ee2ab1d8a7cfe2383d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9cc6e273885d96a9476c8c965c0db5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33b3e5493ca47d91ad45",
+    "url": "./static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "65558011f931e1668c96",
+    "url": "./static/css/main.38301df1.chunk.css"
+  },
+  {
+    "revision": "33b3e5493ca47d91ad45",
+    "url": "./static/js/2.cdf64a5d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.cdf64a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65558011f931e1668c96",
+    "url": "./static/js/main.205ff30d.chunk.js"
+  },
+  {
+    "revision": "217ef14132a50b8f42be",
+    "url": "./static/js/runtime-main.9b14e3b5.js"
+  }
+]);
