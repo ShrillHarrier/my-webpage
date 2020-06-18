c01a0e15dea00229d235efcb827e0423d8ef3be4
Matthew Panizza <38818691+ShrillHarrier@users.noreply.github.com>
2020-06-17 22:20:29 -0400

    Added Contacts

--- src/components/contactpage.js
+++ src/components/contactpage.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import './style.css';
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button, Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Page(){
 	return(
 		<div id="contactpage">
 			<div className="topdiv">
-				<h1>Contact Me:</h1>
+				<h1>Contact Me</h1>
+			</div>
+
+			<div className="flexdiv">
+				<Jumbotron id ="contacttron">
+					<p>Email: <a href="mpanizza@uwaterloo.ca">mpanizza@uwaterloo.ca</a></p>
+					<p>Phone: <a id="phone">+1 901-301-3601</a></p>
+					<p>Github: <a href="https://www.github.com/shrillharrier">View My Github</a></p>
+					<p>Linkedin: <a href="https://www.linkedin.com/in/matthew-panizza-b726a0191/">View My Linkedin</a></p>
+				</Jumbotron>
 			</div>
 		</div>
 		);
