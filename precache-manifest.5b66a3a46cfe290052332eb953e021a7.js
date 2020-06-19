6aecc0ac4e900d1e69b84b5f66fa5d2022c446be
Matthew Panizza <38818691+ShrillHarrier@users.noreply.github.com>
2020-06-19 16:49:33 -0400

    Updates

---
+++ precache-manifest.5b66a3a46cfe290052332eb953e021a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2300565bfc1111e15c0bea9b5daf56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7774a97e45650abaaa87",
+    "url": "/static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "7b002eb47779fc8b3528",
+    "url": "/static/css/main.95071810.chunk.css"
+  },
+  {
+    "revision": "7774a97e45650abaaa87",
+    "url": "/static/js/2.5e8ac1b5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5e8ac1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b002eb47779fc8b3528",
+    "url": "/static/js/main.3ebf9937.chunk.js"
+  },
+  {
+    "revision": "284d0efeb838c30e8049",
+    "url": "/static/js/runtime-main.fce77ebb.js"
+  },
+  {
+    "revision": "3fb02153640447bd277bdcffc67a30cb",
+    "url": "/static/media/WebResume.3fb02153.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a94b8e4b941d3c07375ecbc6701466d1.js"
+  "/precache-manifest.5b66a3a46cfe290052332eb953e021a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
