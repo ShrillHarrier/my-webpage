6aa9918d5e5370a3beb64defec616a3403ca72af
Matthew Panizza <38818691+ShrillHarrier@users.noreply.github.com>
2020-06-20 20:52:50 -0400

    Added fade in animation

--- src/components/aboutpage.js
+++ src/components/aboutpage.js
@@ -14,10 +14,10 @@ function Page(){
 				<div className = "custombreak"></div>
 				<p><b>Langauges:</b> Java, Javascript, Python, C++, C#<br></br><b>Tools:</b> HTML/CSS, ReactJS, BootStrap4, Unity</p>
 		    </Jumbotron>
-			<div className="flexdiv">
+			<div className="flexdiv" id = "aboutborder">
 				<Card>
 					<a href="http://www.uwaterloo.ca/engineering"><img className = "aboutimg" src = "https://www.brocksolutions.com/wp-content/uploads/2020/02/University-of-Waterloo-1080x675.jpg"></img></a>
-					<a href="http://minecraft.net"><img className = "aboutimg" src = "https://i.pinimg.com/474x/57/8f/e1/578fe1e9ac57a62efbf8a746cd4e1aa5.jpg"></img></a>
+					<a href="https://www.callofduty.com/home"><img className = "aboutimg" src = "https://i.pinimg.com/474x/57/8f/e1/578fe1e9ac57a62efbf8a746cd4e1aa5.jpg"></img></a>
 					<a href="http://www.java.com"><img className = "aboutimg" src = "https://miro.medium.com/max/4000/1*_FFpkCWD-KwQmc7oevCRBQ.jpeg"></img></a>						<a href="https://wiki.python.org/moin/TkInter"><img className = "aboutimg" src = "https://learnbatta.com/assets/images/python/python-logo.png"></img></a>
 					<a href="https://github.com/facebook/react"><img className = "aboutimg" src = "https://www.c-sharpcorner.com/article/what-and-why-reactjs/Images/image001.jpg"></img></a>
 				</Card>
