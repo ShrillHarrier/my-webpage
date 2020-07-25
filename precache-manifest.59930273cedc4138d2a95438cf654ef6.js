95e1778ffde53b732900f086f5439a1f8d4ec4b6
Matthew Panizza <38818691+ShrillHarrier@users.noreply.github.com>
2020-07-25 17:25:18 -0400

    Updates

---
+++ precache-manifest.59930273cedc4138d2a95438cf654ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6149f048d0978f3469a75ca99b2aaf0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f16fdc091a58728d036d",
+    "url": "./static/css/2.5f98dec3.chunk.css"
+  },
+  {
+    "revision": "66888e55e39b32f6be39",
+    "url": "./static/css/main.326f20f5.chunk.css"
+  },
+  {
+    "revision": "f16fdc091a58728d036d",
+    "url": "./static/js/2.e8417a55.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "./static/js/2.e8417a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66888e55e39b32f6be39",
+    "url": "./static/js/main.60abd3e4.chunk.js"
+  },
+  {
+    "revision": "217ef14132a50b8f42be",
+    "url": "./static/js/runtime-main.9b14e3b5.js"
+  },
+  {
+    "revision": "6da0f00e0831ac80e0e26fc5d2127d00",
+    "url": "./static/media/WebResume.6da0f00e.pdf"
+  }
+]);
